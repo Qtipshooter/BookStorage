afbb788ddb01adf303302c753c8703b2fc51a1a1
Quinton Graham <quintongraham1@gmail.com>
2025-10-05 11:21:33 -0700

    Added DB Connection stuff

---
+++ backend/util/db_connection.js
@@ -0,0 +1,15 @@
+import { MongoClient } from "mongodb"
+
+const connection_string = process.env.CONNECTION_STRING;
+const client = new MongoClient(connection_string);
+
+// shared connection
+let db_connection = null;
+
+export async function mdb_connect() {
+  if (!db_connection) {
+    await client.connect();
+    db_connection = client.db('');
+  }
+  return db_connection;
+}
