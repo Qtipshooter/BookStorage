89ade25d97226deda13438a1d8bfe28909dcd76a
Quinton Graham <quintongraham1@gmail.com>
2025-10-07 13:33:36 -0700

    Moved Tests to their own file

---
+++ backend/tests/test_users.js
@@ -0,0 +1,171 @@
+import * as users from "../api/users.js"
+
+// ------ TEST FUNCTIONS ------ //
+
+function util_test_result_code(result) {
+  if(result == "pass") {
+    return "[\x1b[32mPass\x1b[0m]"
+  }
+  if(result == "fail") {
+    return "[\x1b[31mFail\x1b[0m]"
+  }
+  return "";
+}
+
+async function test_register_user() {
+  /**Cases to write
+   * successful normal user, duplicate of first user (1 for username, 1 for email),
+   * invalid username (short, long, character, numbers,), invalid email (characters, missing @, additional @,)
+   */
+
+  let passing = true;
+
+  // Function for reading test output regardless of outcome
+  function check_test_register_user (response, pass_cond, test_num){
+    if (response.success == pass_cond){
+      if(response.success) {
+        console.log("#" + test_num + " " + util_test_result_code("pass") + " Registered UserID: " + response.data.user_id);
+      }
+      else {
+        console.log("#" + test_num + " " + util_test_result_code("pass") + " Failed to enter user, message: " + response.error_message);
+      }
+    }
+    else {
+      passing = false;
+      if(response.success) {
+        console.log("#" + test_num + " " + util_test_result_code("fail") + " Registered invalid user: " + response.data.user_id);
+      }
+      else {
+        console.log("#" + test_num + " " + util_test_result_code("fail") + " Failed to enter user, message: " + response.error_message);
+      }
+    }
+  }
+
+  // Tests Array
+  const tests = [
+    {
+      user: "JohnDoe1",
+      email: "john.doe@gmail.com",
+      pass: "Password123",
+      cond: true
+    },
+    {
+      user: "JaneDoe1",
+      email: "jane.doe@gmail.com",
+      pass: "asdfghjkl;'1234567890",
+      cond: true
+    },
+    {
+      user: "JohnDoe",
+      email: "johnathan.doe@geeeze.com",
+      pass: "!@#$%^&*()_+~{}|:\"<>,./;']\\\"aB1", // Inputed Password: !@#$%^&*()_+~{}|:"<>,./;']\"aB1
+      cond: true
+    },
+    {
+      user: "Bobby@Tables",
+      email: "drop+table_child.wah@delete.ahh",
+      pass: "Delete It All!",
+      cond: false
+    },
+    {
+      user: "Sky",
+      email: "so.ar@gmail.com",
+      pass: "more than 1 star in the sky",
+      cond: true
+    },
+  ];
+
+  // Running Tests
+  console.log("-- Testing register_user --");
+  for (let i = 0; i < tests.length; i++) {
+    await users.register_user(tests[i].user, tests[i].email, tests[i].pass).then((res) => {check_test_register_user(res, tests[i].cond, i)});
+  }
+  console.log("-- Test register_user complete --");
+  return passing;
+}
+
+async function test_check_level() {
+  
+}
+
+async function test_get_user() {
+  let passing = true;
+
+  function check_test_get_user(response, pass_cond, test_num) {
+    if(response.success == pass_cond) {
+      if(response.success) {
+        console.log("#" + test_num + " " + util_test_result_code("pass") + " User Obtained: " + response.data.username);
+      }
+      else {
+        console.log("#" + test_num + " " + util_test_result_code("pass") + " User not found");
+      }
+    }
+    else {
+      passing = false;
+      if(response.success) {
+        console.log("#" + test_num + " " + util_test_result_code("fail") + " User Obtained: " + response.data.username);
+      }
+      else {
+        console.log("#" + test_num + " " + util_test_result_code("fail") + " User not found");
+      }
+    }
+  }
+
+  // Tests Array
+  const tests = [
+    {
+      username: "JohnDoe1",
+      cond: true,
+    },
+    {
+      username: "Giberish",
+      cond: false,
+    },
+    {
+      username: "john.doe@gmail.com",
+      cond: true,
+    },
+    {
+      username: "@JohnDoe1",
+      cond: false,
+    },
+  ];
+
+  // Running Tests
+  console.log("-- Testing get_user --");
+  for (let i = 0; i < tests.length; i++) {
+    await users.get_user(tests[i].username).then((res) => {check_test_get_user(res, tests[i].cond, i)});
+  }
+  console.log("-- Test get_user complete --");
+
+  return passing;
+}
+
+async function test_get_users() { 
+  let passing = true;
+  
+  function check_test_get_users(response) {
+    if(response.success) {
+      console.log(util_test_result_code("pass") + " Users Found:")
+      response.data.forEach(user => {
+        console.log(JSON.stringify(user));
+      });
+    }
+    else {
+      passing = false;
+      console.log(util_test_result_code("fail") + " Users not Fetched")
+    }
+  }
+  console.log("-- Testing get_users --");
+  await users.get_users().then((res) => {check_test_get_users(res)});
+  console.log("-- Test get_users complete --");
+  return passing;
+}
+
+
+// ------ Run Tests ------ //
+await test_register_user();
+await test_get_user();
+await test_get_users();
+// await test_check_level();
+process.exit()
