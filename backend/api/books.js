194ddf85894728ea6ea34cfb34f1da500ca9c39e
Quinton Graham <quintongraham1@gmail.com>
2025-10-15 18:51:47 -0700

    Started work on add_book function

--- backend/api/books.js
+++ backend/api/books.js
@@ -3,6 +3,7 @@
 // Book db functions for Book Storage Program
 
 import { mdb_connect } from "../util/db_connection";
+import { get_ObjectID } from "./util";
 
 /** add_book
  * Add a book to the collection
@@ -10,7 +11,62 @@ import { mdb_connect } from "../util/db_connection";
  * @param {Object} book
  * @return {Promise<Object>}
  */
-async function add_book(user_id, book) {}
+async function add_book(user_id, book) {
+  // Init
+  const db = mdb_connect();
+  const books = db.collection("books");
+  const oid = get_ObjectID(user_id);
+  const check_isbn_10 = /^{0-9}[10]$/
+  const check_isbn_13 = /^{0-9}[13]$/
+  let new_book = {};
+
+  // Check oid
+  if(!oid) {
+    return {
+      success: false,
+      error_message: "Invalid User ID supplied",
+    }
+  }
+
+  // Check book object and copy over to new_book (sanitation)
+  if(book) {
+
+    // Required items
+    if(typeof book.title === string) {new_book.title = book.title;}
+    if(book.authors && book.authors instanceof Array){
+      new_book.authors = []
+      book.authors.forEach(author => {
+        if(typeof author === string) {new_book.authors.push(author)}
+      });
+    }
+    if(book.genres && book.genres instanceof Array){
+      new_book.genres = []
+      book.genres.forEach(genre => {
+        if(typeof genre === string) {new_book.genres.push(genre);}
+      });
+    }
+    if(typeof book.price === number){ new_book.price = book.price;}
+    if(typeof book.description === string){ new_book.description = book.description;}
+    if(typeof book.isbn_10 === number){
+      if(book.isbn_10.match(check_isbn_10)) {
+        new_book.isbn_10 = book.isbn_10;
+      }
+    }
+    if(typeof book.isbn_13 === number){
+      if(book.isbn_13.match(check_isbn_13)) {
+        new_book.isbn_13 = book.isbn_13;
+      }
+    }
+    // Non Required Items
+  }
+
+  // Check for duplicate unique identifiers (ISBN)
+
+  // Insert book
+
+  // Return result
+
+}
 
 /** update_book
  * Updates a book from owner or admin.  Only updates supplied fields, overwrites supplied fields
