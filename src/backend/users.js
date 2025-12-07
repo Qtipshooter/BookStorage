b28702ee4cec2a6b9fdd426ee2afb2785767bbc7
Quinton Graham <quintongraham1@gmail.com>
2025-12-07 12:25:34 -0800

    Users Error Updates

--- src/backend/users.js
+++ src/backend/users.js
@@ -1,64 +1,75 @@
-import dotenv from "dotenv"
-import bcrypt from "bcrypt";
-import { BS_Error, mdb_connect, failure, get_ObjectID, success } from "./util.js";
-const envpath = `.env${process.env.NODE_ENV || ""}`;
-dotenv.config({ path: envpath })
+// users.js
+// Quinton Graham
+// Handles user functions for the Book Storage Program
 
-// ------ User Functions ------ //
-/** register_user
- * Performs first time registration for a user, including setting up their user account and
- * their data in the library db.
- * @param {string} username Username to register account with
- * @param {string} email Email to register account with
- * @param {string} password Clear password to resiter account with
- * @return {Promise<Object>} On success, the id of the newly created user, on failure, error_message
+import { BS_Error, get_ObjectID, mdb_connect, } from "./util.js";
+
+/**
+ * Adds a new user and initializes their account.
+ * @param {string} username The username for the new user
+ * @param {string} email The email to be associated with the account
+ * @param {string} password The password to sign up with
+ * @return {Promise<Object>} On successful registration, returns the created user object, otherwise returns an error object
  */
-async function register_user(username, email, password) {
+export async function register_user(username, email, password) {
   // Init
   const username_regex = /^\w{3,64}$/i; // Username -> 3-64 alphanumeric characters, underscores yes spaces no
   const email_regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i; // Email -> accepts any valid email (and maybe some invalid ones too :P)
   const pass_regex = /^(?=.*[A-Za-z])(?=.*\d).{8,128}$/; // Password -> 8-128 with a character and number assertation
   const db = await mdb_connect(); // Database connection
   const users = db.collection("users"); // User table
-  const libraries = db.collection("libraries"); // Library Table
   const c_date = new Date(); // Creation Timestamp
+  const hash_passes = 12;
+  let user_id;
 
   // Legal entry checks
-  if (!username.match(username_regex)) { throw new BS_Error(BS_Error.ERR.INVALID_FORMAT, "Username contains illegal characters or length is invalid");}
-  if (!email.match(email_regex)) { throw new BS_Error(BS_Error.ERR.INVALID_FORMAT, "Email is invalid");}
-  if (!password.match(pass_regex)) { throw new BS_Error(BS_Error.ERR.INVALID_FORMAT, "Password does not meet length or complexity requirements");}
+  if (!username.match(username_regex)) { return { error: "Username contains illegal characters or length is invalid" }; }
+  if (!email.match(email_regex)) { return { error: "Email is invalid" }; }
+  if (!password.match(pass_regex)) { return { error: "Password does not meet length or complexity requirements" }; }
 
   // Already in use checks
-  if (await users.findOne({ username: username.toLowerCase() })) { throw new BS_Error(BS_Error.ERR.DUPLICATE_DATA, "Username in use");}
-  if (await users.findOne({ email: email.toLowerCase() })) { throw new BS_Error(BS_Error.ERR.DUPLICATE_DATA, "Email in use");}
+  if (await users.findOne({ username: username.toLowerCase() })) { return { error: "Username in use" } }
+  if (await users.findOne({ email: email.toLowerCase() })) { return { error: "Email in use" } }
 
   // Checks complete, insert
   // NOTE TO SELF: Display name is stored seperately due to the size concern 
   // being less than the performance concern when searching at scale.
-  const passhash = await bcrypt.hash(password, Number(process.env.HASH_PASSES));
-  const user_id = await users.insertOne({
+  const passhash = await bcrypt.hash(password, hash_passes);
+  try {
+    user_id = await users.insertOne({
+      display_name: username,
+      username: username.toLowerCase(),
+      email: email.toLowerCase(),
+      hashcode: passhash,
+      created_date: c_date,
+      level: "user"
+    }).then((res) => {
+      return res.insertedId;
+    })
+  }
+  catch (e) {
+    console.log("Error registering user: " + e.message);
+    return { error: "Server error while registering user" }
+  }
+
+  // Success Payload
+  return {
+    _id: user_id.toHexString(),
     display_name: username,
     username: username.toLowerCase(),
     email: email.toLowerCase(),
-    hashcode: passhash,
-    created_date: c_date,
-    level: "user"
-  }).then((res) => {
-    return res.insertedId;
-  })
-
-  libraries.insertOne({ user_id: user_id })
-
-  // Success Payload
-  return user_id;
+    level: "user",
+  };
 }
 
-/** get_level
+/**
  * Checks the user level (admin/user/etc.)
  * @param {string} user_id The id of the user to check
- * @return {Promise<Object>} If found, the level of the user, otherwise error_message
+ * @return {Promise<string>} If found, the level of the user, otherwise null
+ * @throws {BS_Error} Throws BS_Error if user_id is an invalid format
+ * @todo Change to not throw and instead log with logging function
  */
-async function get_level(user_id) {
+export async function get_level(user_id) {
   // Init
   const db = await mdb_connect();
   const users = db.collection("users");
@@ -67,34 +78,113 @@ async function get_level(user_id) {
   // Verification
   if (!ob_id) { throw new BS_Error(BS_Error.ERR.INVALID_OBJECT); }
 
-  // Fetch
-  const level = await users.findOne({ _id: ob_id }).then((res) => {
-    if (res) {
-      return res.level;
-    }
-    return res;
-  })
-
-  // Results
-  if (level) { return level; }
-  throw new BS_Error(BS_Error.ERR.DATA_NOT_FOUND);
+  // Fetch and return
+  return await users.findOne({ _id: ob_id }).then((res) => {
+    if (res) { return res.level; }
+    return null;
+  });
 }
 
-//TODO update the return to have object rather than just id
-/** authorize_user
- * Verifies login information
+/**
+ * Gets a user object by their username or email
+ * @param {string} username The username or email of the user to get info for
+ * @return {Promise<Object>} On success, the user object, on failure, error message
+ */
+export async function get_user(username) {
+  // Init
+  if (typeof username != "string") { return { error: "Username must be a string" } }
+  const is_email = username.indexOf("@") > 0; // Check if email
+  const db = await mdb_connect(); // DB Con
+  const users = db.collection("users"); // User table
+  const proj = { hashcode: 0, } // Projection to exclude pass-hashes
+  const user = null;
+
+  // Find the User
+  if (is_email) { user = await users.findOne({ email: username.toLowerCase() }, { projection: proj }) }
+  else { user = await users.findOne({ username: username.toLowerCase() }, { projection: proj }) }
+
+  if (!user) { user = { error: "User not found" } }
+  return user;
+}
+
+/**
+ * Gets a user object by their ID
+ * @param {string} user_id The user id to search for
+ * @return {Promise<Object>} On success, the user object, on failure, error message
+ */
+export async function get_user_by_id(user_id) {
+  // Init
+  const db = mdb_connect();
+  const users = db.collection("users");
+  const proj = { hashcode: 0 };
+  const user_oid = get_ObjectID(user_id);
+
+  if (!user_oid) { return { error: "Invalid User ID" } }
+
+  // Find the User
+  const user = await users.findOne({ _id: user_oid }, { projection: proj });
+  if (user) { return user }
+  return { error: "User not Found" }
+}
+
+/**
+ * Gets all users in database with optional parameters.  Use result.length to verify return is not error.
+ * @param // Todo
+ * @return {Promise<Object>} An array of the users found, or null on failure
+ */
+export async function get_users() {
+  // Init
+  const db = mdb_connect();
+  const users = db.collection("users");
+  const proj = { hashcode: 0 };
+
+  // Fetch Users
+  const user_col = await users.find({}, { projection: proj }).toArray();
+
+  if (user_col.length) { return user_col }
+  return null;
+}
+
+/**
+ * Removes a user account
+ * @param {string} user_id The user ID of the account to delete
+ * @return {Promise<boolean>} True on normal operation, false on invalid param, invalid user, or other db errors
+ * @todo Need to add functionality of deleting the libraries associated with user
+ * @todo Need to add removal of user from all associated books or annonmize the information
+ * @todo Convert console logs to logging program
+ */
+export async function remove_user(user_id) {
+  // Init
+  const db = await mdb_connect();
+  const users = db.collection("users");
+  const obj_id = get_ObjectID(user_id);
+  if (!obj_id) { return false; } // Invalid ID
+
+  //
+  const result = await users.deleteOne({ _id: obj_id });
+  if (result.acknowledged) { return true; }
+  console.log("Error deleting account with ID: " + user_id);
+  return false;
+}
+
+/**
+ * Verifies login information and gets data for login if valid
  * @param {string} username The username or email of the user to check
  * @param {string} password The cleartext password of the user
- * @return {Promise<Object>} On success, the user object, on failure, error_message
+ * @return {Promise<Object>} On success, the user object, on failure, error message
  */
-async function authorize_user(username, password) {
+export async function authorize_user(username, password) {
+  // Verify input
+  if (typeof username !== "string" || typeof password != "string") {
+    return { error: "Username/Password must be supplied as strings" }
+  }
+
   // Init
-  if (typeof username !== "string" || typeof password != "string") { throw new BS_Error(BS_Error.ERR.INVALID_FORMAT, "Username/Password must be supplied as strings"); }
   const isEmail = username.indexOf("@") > 0; // Check if email
   const db = await mdb_connect(); // DB Con
   const users = db.collection("users"); // User Table
-  let user;
-  let authenticated = false;
+  let user; // User object to return
+  let authenticated = false; // Auth Status
 
   // Get User
   if (isEmail) { user = await users.findOne({ email: username }); }
@@ -102,107 +192,17 @@ async function authorize_user(username, password) {
 
   // Check Hash
   if (user) { authenticated = await bcrypt.compare(password, user.hashcode); }
-  else { throw new BS_Error(BS_Error.ERR.DATA_NOT_FOUND, "User not found"); }
+  else { return { error: "User Not Found" } }
 
   // Return payload
-  if (authenticated) { return user._id.toString(); }
-  throw new BS_Error(BS_Error.ERR.UNAUTHORIZED, "Invalid Password");
-}
-
-/** get_user
- * Gets basic user information
- * @param {string} username The username or email of the user to get info for
- * @return {Promise<Object>} On success, the user object, on failure, error_message
- */
-async function get_user(username) {
-  // Init
-  if (typeof username !== "string") { throw new BS_Error(BS_Error.ERR.INVALID_FORMAT, "Username/Email is not a string"); }
-  const isEmail = username.indexOf("@") > 0; // Check if email
-  const db = await mdb_connect(); // DB Con
-  const users = db.collection("users"); // User table
-  const proj = { hashcode: 0, } // Projection to exclude pass-hashes
-  let user = null; // User object, not directly returned so that we have the success state
-
-  // Find the User
-  if (isEmail) { user = await users.findOne({ email: username.toLowerCase() }, { projection: proj }) }
-  else { user = await users.findOne({ username: username.toLowerCase() }, { projection: proj }) }
-
-  // Return Payload
-  if (user) { return user; }
-  throw new BS_Error(BS_Error.ERR.DATA_NOT_FOUND, "User Not Found");
-}
-
-/** get_user_by_id
- * Gets basic user information
- * @param {string} user_id The ID of the user to get info for
- * @return {Promise<Object>} On success, the user object, on failure, error_message
- */
-async function get_user_by_id(user_id) {
-  // Init
-  const db = await mdb_connect(); // DB Con
-  const users = db.collection("users"); // User table
-  const proj = { hashcode: 0, } // Projection to exclude pass-hashes
-  const user_oid = get_ObjectID(user_id);
-  let user = null; // User object, not directly returned so that we have the success state
-
-  // Find the User
-  if (user_oid) { user = await users.findOne({ _id: user_oid }, { projection: proj }); }
-  else { throw new BS_Error(BS_Error.ERR.INVALID_OBJECT); }
-
-  // Return Payload
-  if (user) { return user; }
-  throw new BS_Error(BS_Error.ERR.DATA_NOT_FOUND, "User Not Found");
-}
-
-/** get_users
- * Gets basic user information for all users (administrative)
- * @return {Promise<Object>} Returns an array of all the users
- */
-async function get_users() {
-  // Init
-  const db = await mdb_connect(); // DB Con
-  const users = db.collection("users"); // User table
-  const proj = { hashcode: 0, } // Projection to exclude pass-hashes
-
-  // Fetch users
-  const user_col = await users.find({}, { projection: { hashcode: 0 } }).toArray();
-
-  // Return Payload
-  if (user_col) { return user_col; }
-  throw new BS_Error(BS_Error.ERR.UNKNOWN);
-}
-
-//TODO Implement anonymizing
-/** remove_user
- * Removes user from database and anonymizes or deletes connected data
- * @param {string} user_id ID of the user to remove
- * @return {Promise<Object>} Returns the id of removed user, or error_message on failure
- */
-async function remove_user(user_id) {
-  // Init
-  const db = await mdb_connect();
-  const users = db.collection("users");
-  const obj_id = get_ObjectID(user_id);
-  if (!obj_id) { throw new BS_Error(BS_Error.ERR.INVALID_OBJECT); }
-
-  // TODO Anonymize data once other endpoints are created
-
-  // Removal from database
-  const result = await users.deleteOne({ _id: obj_id });
-
-  if (result.acknowledged) {
-    if (result.deletedCount) { return user_id; }
-    else { throw new BS_Error(BS_Error.ERR.DATA_NOT_FOUND, "No Matching Users"); }
+  if (authenticated) {
+    return {
+      _id: user._id,
+      display_name: user.display_name,
+      username: user.username,
+      email: user.email,
+      level: user.level,
+    };
   }
-  throw new BS_Error(BS_Error.ERR.UNKNOWN);
-}
-
-export {
-  register_user,
-  get_level,
-  get_user,
-  get_user_by_id,
-  get_users,
-  remove_user,
-  authorize_user,
+  return { error: "Invalid Password" };
 }
