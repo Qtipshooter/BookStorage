84b0b84111149120ae54c01fa660bc07859565dc
Quinton Graham <quintongraham1@gmail.com>
2025-12-07 20:11:57 -0800

    Books Error Updates and Function Changes

--- src/backend/books.js
+++ src/backend/books.js
@@ -3,322 +3,196 @@
 // Book db functions for Book Storage Program
 
 import { mdb_connect } from "./util.js";
-import { get_level, get_user, get_user_by_id } from "./users.js";
 import { BS_Error, get_ObjectID, sanitize_book, } from "./util.js";
+import { get_level, get_user, get_user_by_id } from "./users.js";
 
-/** add_book
- * Add a book to the database
+/**
+ * Adds a book to the database
  * @param {string} user_id The user adding the book
  * @param {Object} book The data of the book to add
- * @return {Promise<Object>} The ID of the newly created book
+ * @return {Promise<string>} The ID of the newly created book
+ * @throws {BS_Error || WriteError || WriteConcernError} Throws if required data is missing, if there is only 
+ *   invalid data, if there is a duplicate book, or if there is an error inserting the book
  */
-async function add_book(user_id, book) {
+export async function add_book(user_id, book) {
   // Init
   const db = await mdb_connect();
   const books = db.collection("books");
   const oid = get_ObjectID(user_id);
   if (!oid) { throw new BS_Error(BS_Error.ERR.INVALID_OBJECT, "Invalid User ID"); }
-  let user;
-  let duplicate = null;
+  const user = get_user_by_id(user_id);
   let new_book = sanitize_book(book);
 
   // Check input params
-  try { user = await get_user_by_id(user_id) }
-  catch (e) { if (e instanceof BS_Error) { throw e } else { throw new BS_Error(BS_Error.ERR.UNKNOWN, "Error getting user"); } }
-  if (!new_book) { throw new BS_Error(BS_Error.ERR.INVALID_FORMAT, "Invalid Book Data Supplied"); }
-  new_book.user_id = oid;
-
-  // Verify necessary parameters are included
+  if (!user._id) { throw new BS_Error(BS_Error.ERR.UNKNOWN, "Error verifying user ID", user); }
+  if (!new_book) { throw new BS_Error(BS_Error.ERR.MISSING_DATA, "No Valid Book Data Supplied"); }
   if (!new_book.title || !new_book.authors) { throw new BS_Error(BS_Error.ERR.MISSING_DATA, "Missing required Book Data (Title or Authors)"); }
+  if (duplicate_exists(isbn_10 = new_book.isbn_10, isbn_13 = new_book.isbn_13)) { throw new BS_Error(BS_Error.ERR.DUPLICATE_DATA, "Duplicate ISBN provided"); }
 
-  // Check for duplicate unique identifiers (ISBN)
-  if (new_book.isbn_10) {
-    duplicate = await books.findOne({ isbn_10: new_book.isbn_10 })
-    if (duplicate) { throw new BS_Error(BS_Error.ERR.DUPLICATE_DATA, "Duplicate ISBN-10"); }
-  }
-  if (new_book.isbn_13) {
-    duplicate = await books.findOne({ isbn_13: new_book.isbn_13 })
-    if (duplicate) { throw new BS_Error(BS_Error.ERR.DUPLICATE_DATA, "Duplicate ISBN-13"); }
-  }
-
-  // Insert book
-  try { return await books.insertOne(new_book).then((res) => { return res.insertedId; }) }
-  catch (err) { throw new BS_Error(BS_Error.ERR.UNKNOWN, "Error inserting Book"); }
+  // Insert Book
+  new_book.user_id = oid;
+  return books.insertOne(new_book).then((res) => { return res.insertedId; })
 }
 
-/** update_book
- * Updates a book from owner or admin.  Only updates supplied fields, overwrites supplied fields
- * @param {string} user_id The ID of the user updating the book.  Must be owner or admin
+/** 
+ * Updates a book's information.  Overwrites supplied fields, does not touch others
  * @param {string} book_id The ID of the book to update
  * @param {Object} updated_book_values The updated field/value pairs
- * @return {Promise<Object>} Returns the updated book count, or error_message on failure
+ * @return {Promise<Object>} Returns the book on success
+ * @throws {BS_Error || WriteError || WriteConcernError} Throws if there is invalid data or there is an error inserting the book
  */
-async function update_book(user_id, book_id, updated_book_values) {
+export async function update_book(book_id, updated_book_values) {
   // Init
   const db = await mdb_connect();
   const books = db.collection("books")
   const sanitized_updates = sanitize_book(updated_book_values);
-  const u_id = get_ObjectID(user_id);
-  let book;
-  try { book = await get_book(book_id); }
-  catch (e) { throw new BS_Error(BS_Error.ERR.DATA_NOT_FOUND, "Book not Found"); }
-  const owner = book.user_id;
-  let sub_result;
+  let book = await get_book(book_id);
 
-  // Error on invalid params
-  if (!(u_id && sanitized_updates)) { throw new BS_Error(BS_Error.ERR.INVALID_FORMAT, "Invalid Parameter(s)"); }
-
-  // Check duplicated Data
-  if (sanitized_updates.isbn_10) {
-    // TODO Check for duplicate
-    let duplicate_found = false;
-
-    try {
-      const sub_result = await find_books({ isbn_10: sanitized_updates.isbn_10 });
-      if (sub_result.length > 0) { duplicate_found = true; }
-    }
-    catch (e) { // No Results
-      if (!(e instanceof BS_Error)) { throw e; } // TODO: Change function structures more
-    }
-    if (duplicate_found) { throw new BS_Error(BS_Error.ERR.DUPLICATE_DATA, "Duplicate ISBN Number supplied", sanitized_updates.isbn_10); }
-  }
-  if (sanitized_updates.isbn_13) {
-    // TODO Check for duplicate
-    let duplicate_found = false;
-
-    try {
-      const sub_result = await find_books({ isbn_13: sanitized_updates.isbn_13 });
-      if (sub_result.length > 0) { duplicate_found = true; }
-    }
-    catch (e) { // No Results
-      if (!(e instanceof BS_Error)) { throw e; } // TODO: Change function structures more
-    }
-    if (duplicate_found) { throw new BS_Error(BS_Error.ERR.DUPLICATE_DATA, "Duplicate ISBN Number supplied", sanitized_updates.isbn_13); }
+  // Verify request
+  if (!sanitized_updates) { throw new BS_Error(BS_Error.ERR.MISSING_DATA, "No Valid Updates Supplied"); }
+  if (!book) { throw new BS_Error(BS_Error.ERR.DATA_NOT_FOUND, "Book not Found"); }
+  if (await duplicate_exists(isbn_10 = sanitized_updates.isbn_10, isbn_13 = sanitized_updates.isbn_13)) {
+    throw new BS_Error(BS_Error.ERR.DUPLICATE_DATA, "Duplicate ISBN supplied");
   }
 
   // Sanitize anything the sanatizer didn't grab (Other function for user, never update _id)
   delete sanitized_updates._id;
   delete sanitized_updates.user_id;
 
-  // Check user validity of request
-  if (!(owner.equals(u_id))) {
-    let user_ver = await get_level(user_id);
-    if (!(user_ver == "admin")) { throw new BS_Error(BS_Error.ERR.UNAUTHORIZED, "User not authorized to edit this book"); }
-  }
-
-
-
-  // Process Updates
+  // Process updates
   const result = await books.updateOne({ _id: book._id }, { $set: sanitized_updates });
-  if (result.modifiedCount > 0) { return result.modifiedCount; }
+  if (result.modifiedCount > 0) { return get_book(book_id); }
   throw new BS_Error(BS_Error.ERR.UNKNOWN, "No Updates Processed");
+
 }
 
-/** update_book_owner
- * Updates owner from current user to new user or default user/admins only
- * @param {string} book_id The ID of the book to update the owner of
- * @param {string} current_user The owner/admin of the book to authorize
- * @param {string} new_username The new username to update the owner to
- * @return {Promise<Object>}
+/**
+ * Updates owner for a book to a new user (@todo or blank/default)
+ * @param {string} book_id The ID of the book to update
+ * @param {string} new_username The username of the new user
+ * @return {Promise<>} The updated book
+ * @throws {BS_Error || WriteError || WriteConcernError} Throws if the user is invalid or there is an error inserting the book
  */
-async function update_book_owner(book_id, current_user, new_username) {
+export async function update_book_owner(book_id, new_username) {
   // Init
   const db = await mdb_connect();
   const books = db.collection("books");
-  let request_validated = false;
-  let new_user;
-  try { new_user = await get_user(new_username); }
-  catch (e) { throw new BS_Error(BS_Error.ERR.DATA_NOT_FOUND, "Invalid New User"); }
-  const new_id = new_user._id;
-  let book;
-  try { book = await get_book(book_id); }
-  catch (e) { throw new BS_Error(BS_Error.ERR.DATA_NOT_FOUND, "Invalid Book"); }
+  const u_id = await get_user(new_username)._id;
   const bo_id = get_ObjectID(book_id);
-  const book_owner = book.user_id;
-  let user_level;
-  try { user_level = await get_level(current_user); }
-  catch (e) { user_level = "user"; }
 
-  // Request Validation
-  if (user_level == "admin") { request_validated = true; }
-  if (current_user == book_owner) { request_validated = true; }
-  if (!request_validated) { throw new BS_Error(BS_Error.ERR.UNAUTHORIZED, "Invalid User, must be book owner or an admin"); }
+  // Verify Input
+  if (!u_id) { throw new BS_Error(BS_Error.ERR.DATA_NOT_FOUND, "User not Found"); }
+  if (!bo_id) { throw new BS_Error(BS_Error.ERR.INVALID_OBJECT, "Invalid Book ID") }
 
   // Update Book
-  const book_update_result = await books.updateOne({ _id: bo_id }, { $set: { user_id: new_id } });
-  if (book_update_result.modifiedCount) { return book_update_result.modifiedCount; }
-  throw new BS_Error(BS_Error.ERR.UNKNOWN, "Error updating book owner", {
-    new_id: new_id,
-    book: book,
-
-  });
+  let result = await books.updateOne({ _id: bo_id }, { $set: { user_id: u_id } })
+  if (result.modifiedCount > 0) { return get_book(book_id); }
+  throw new BS_Error(BS_Error.ERR.UNKNOWN, "Error updating book owner", result);
 }
 
-/** delete_book
- * Deletes a book from db
- * !Constraints -> requestor has to be owner, and book must be in no other collections OR 
- * requestor has to be admin and will delete from all collections.
- * @param {string} user_id The user authorizing the delete (owner/admin)
+/** @todo Add options: delete from all libs, delete only if unattached */
+/** 
+ * Deletes a book from the database
  * @param {string} book_id The ID of the book to delete
- * @return {Promise<Object>} Returns the deleted count or error_message on failure
+ * @return {Promise<boolean>} true on success, false on failure
  */
-async function delete_book(user_id, book_id) {
-  // !TODO! Add owner delete own book if it is only in their own library w/ override by admin
+export async function delete_book(book_id) {
   // Init
   const db = await mdb_connect();
   const books = db.collection("books");
-  let book;
-  try { book = await get_book(book_id); }
-  catch (e) { throw new BS_Error(BS_Error.ERR.DATA_NOT_FOUND, "Book Not Found"); }
   const bo_id = get_ObjectID(book_id);
-  let user_level;
-  try { user_level = await get_level(user_id); }
-  catch (e) { user_level = "user"; }
-  const owner = book.user_id;
-  const uo_id = get_ObjectID(user_id);
 
-  // Identity Verification
-  if (!(owner.equals(uo_id))) {
-    if (!(user_level == "admin")) {
-      throw new BS_Error(BS_Error.ERR.UNAUTHORIZED);
-    }
-  }
+  /** @todo Check libraries for book **/
+  // -> Function call to check for book in libraries
+  // -> logic based on option to delete from all
 
-  // Delete
-  const result = await books.deleteOne({ _id: bo_id });
-  if (result.deletedCount > 0) { return result.deletedCount; }
-  throw new BS_Error(BS_Error.ERR.UNKNOWN, "No Books Deleted");
+  const result = await books.deleteOne({ _id: bo_id })
+  return result.deletedCount > 0;
 }
 
-/** get_book
+/**
  * Gets book data by supplied ID
  * @param {string} book_id The ID of the book to find
- * @return {Promise<Object>} Book object data or error_message on failure
+ * @return {Promise<Object>} Book object data or null
  */
-async function get_book(book_id) {
+export async function get_book(book_id) {
   // Init
   const db = await mdb_connect();
   const books = db.collection("books");
   const obj_id = get_ObjectID(book_id);
 
-  // Verify ID
-  if (!obj_id) { throw new BS_Error(BS_Error.ERR.INVALID_OBJECT, "Invalid Book ID"); }
-
-  // Get Book
-  const book = await books.findOne({ _id: obj_id });
-  if (book) { return book; }
-  throw new BS_Error(BS_Error.ERR.DATA_NOT_FOUND);
+  // Fetch Book
+  if (!obj_id) { return null; }
+  return books.findOne({ _id: obj_id });
 }
 
-/** get_books
- * Gets all the books in the database
- * @param {string[]} [fields = null] Fields to return (Null for all)
- * @param {number} limit Limit of results to return (0 for no limit)
- * @param {string} sort_field Name of field to sort by (title by default)
- * @param {boolean} ascending Sorting ascending or descending (ascending by default)
- * @return {Promise<Object>} An array of all the book data returned with options selected, or error_message on failure
+/**
+ * Fetches a set of books
+ * @param {string} [search_term=null] Optional Term to search all fields for
+ * @param {string[]} [fields=null] Optional Array of fields to return (all by default)
+ * @param {number} [limit=0] Optional Limit of results returned
+ * @param {string} [sort_field="title"] Optional the field the results will be sorted on
+ * @param {boolean} [ascending=true] Optional Sort direction (ascending by default)
+ * @return {Promise<Object>} An array of the books returned from options, or null on no results
  */
-async function get_books(fields = null, limit = 0, sort_field = "title", ascending = true) {
+export async function get_books(search_term = null, fields = null, limit = 0, sort_field = "title", ascending = true) {
   // Init
   const db = await mdb_connect();
-  const books = db.collection("books");
+  const books = await db.collection("books");
   const book_fields = [
     "title",
+    "user_id",
     "authors",
     "genres",
     "description",
     "isbn_10",
     "isbn_13",
-  ]
-  let find_options = { projection: { _id: 1 }, limit: 0 }
-  let sort_option = {}
+  ];
+  let find_options = { projection: {}, limit: 0 };
+  let sort_options = {};
+  let query = {};
 
-  // Verify data or default
-  if (!fields) { fields = ["all"]; }
-  if (fields && !(fields instanceof Array)) { throw new BS_Error(BS_Error.ERR.INVALID_FORMAT, "Invalid fields format applied"); }
-  else if (fields.includes("all")) { fields = book_fields.slice(); }
-  if (limit && (Number(limit) == NaN || Number(limit < 0))) { throw new BS_Error(BS_Error.ERR.INVALID_FORMAT, "Invalid limit supplied"); }
+  // Apply Search
+  if (search_term && typeof search_term == "string") {
+    query = { "$or": [] };
+    for (let i = 0; i < book_fields.length; i++) {
+      let field_object = {};
+      field_object[book_fields[i]] = { "$regex": search_term, "$options": "i" };
+      query.$or.push(field_object);
+    }
+  }
+
+  // Verify/Default options
+  if (!(fields instanceof Array)) { fields = book_fields.slice(); }
+  for (let i = 0; i < fields.length; i++) { if (book_fields.includes(fields[i])) { find_options.projection[fields[i]] = 1 } }
+  if (limit && !(Number(limit) === NaN || Number(limit) < 0)) { find_options.limit = Number(limit); }
   if (!book_fields.includes(sort_field)) { sort_field = "title"; }
-  if (ascending) { sort_option[sort_field] = 1; }
-  else { sort_option[sort_field] = -1; } // Descending
+  if (ascending) { sort_options[sort_field] = 1; }
+  else { sort_options[sort_field] = -1; }
 
-  // Parse Data
-  for (let i = 0; i < fields.length; i++) {
-    if (book_fields.includes(fields[i])) { find_options.projection[fields[i]] = 1; }
-  }
-  find_options.limit = limit;
+  // Fetch and Return
+  const response = await books.find(query, find_options).sort(sort_options).toArray();
+  if (!response.length) { return null; }
+  return response;
 
-
-  // Fetch Data
-  const response = await books.find({}, find_options).sort(sort_option).toArray();
-  if (!response.length) { throw new BS_Error(BS_Error.ERR.DATA_NOT_FOUND, "No Results", response); }
-
-  // Return payload
-  return response
 }
 
-/** search_books
- * Simple search based on one search term looking through all available fields
- * @param {string} search_term String of term to search through database for
- * @return {Promise<Object>} Cursor to all books matching the search term, or error_message on failure
+/**
+ * Checks for duplicate books based on values
+ * @param {string} [isbn_10=null] Value to check for duplicates
+ * @param {string} [isbn_13=null] Value to check for duplicates
+ * @returns {Promise<boolean>} true if there is a duplicate, else false
  */
-async function search_books(search_term) {
+export async function duplicate_exists(isbn_10 = null, isbn_13 = null) {
   // Init
   const db = await mdb_connect();
   const books = db.collection("books");
-  const book_fields = [
-    "title",
-    "authors",
-    "genres",
-    "description",
-    "isbn_10",
-    "isbn_13",
-  ]
-  let query = { "$or": [] };
 
-  // Verify Search Term
-  if (!(typeof search_term === "string")) { throw new BS_Error(BS_Error.ERR.INVALID_FORMAT, "Search term is not a string"); }
+  if (typeof isbn_10 == "string") { if (await books.findOne({ isbn_10: isbn_10 })) { return true; } }
+  if (typeof isbn_13 == "string") { if (await books.findOne({ isbn_13: isbn_13 })) { return true; } }
 
-  // Build Query
-  for (let i = 0; i < book_fields.length; i++) {
-    let field_object = {}
-    field_object[book_fields[i]] = {
-      "$regex": search_term,
-      "$options": "i"
-    }
-    query.$or.push(field_object);
-  }
-
-  // Find matching books
-  const response = await books.find(query).toArray();
-  if (!response.length) { throw new BS_Error(BS_Error.ERR.DATA_NOT_FOUND, "No Results Found"); }
-
-  // Return payload
-  return response;
+  // No Duplicates Found
+  return false;
 }
 
-/** find_books
- * Finds book based on specific fields being searched
- * @param {Object} search_book Book formated like an updated_book_values object
- * @param {boolean} [exact=false] Only exact matches on given fields
- * @return {Promise<Object>} Cursor to all books matching the search term, or error_message on failure
- */
-async function find_books(search_book, exact = false) {
-  // Init
-  const db = await mdb_connect();
-  const books = db.collection("books")
-  const sanitized_book = sanitize_book(search_book);
-  if (!sanitized_book) { throw new BS_Error(BS_Error.ERR.INVALID_FORMAT, "Invalid search book"); }
-
-  if (!exact) {
-    for (const key in sanitized_book) {
-      sanitized_book[key] = RegExp(sanitized_book[key], "i");
-    }
-  }
-
-  const response = await books.find(sanitized_book).toArray();
-
-  return response;
-}
-
-export { add_book, update_book, update_book_owner, delete_book, get_book, search_books, get_books }
+/** @todo Advanced Search (by each field) */
